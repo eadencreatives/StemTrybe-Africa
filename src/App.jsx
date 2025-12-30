bbd51b9a293c72d2f2356f33d90f9b6f7ab7584a
eadencreatives <eadencre8ives@gmail.com>
2025-12-30 16:32:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,16 @@ import Courses from "./pages/Courses";
 import Blog from "./pages/Blog";
 import About from "./pages/About";
 import ProtectedRoute from "./components/ProtectedRoute";
+import CourseLayout from "./pages/CourseLayout"; // NEW: Course layout with module sidebar
+import ModulePage from "./pages/ModulePage";     // NEW: Individual module content
 
 // Layout wrapper to conditionally show Navbar
 function Layout({ children }) {
   const location = useLocation();
   
   // Don't show Navbar on login and dashboard pages
-  const hideNavbar = location.pathname === '/login' || location.pathname === '/dashboard';
+  const hideNavbar = location.pathname === '/login' || 
+                     location.pathname.startsWith('/dashboard');
   
   return (
     <>
@@ -33,6 +36,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/login" element={<Login />} />
+          
+          {/* Protected Dashboard Routes with Nested Course Navigation */}
           <Route
             path="/dashboard"
             element={
@@ -40,7 +45,11 @@ function App() {
                 <Dashboard />
               </ProtectedRoute>
             }
-          />
+          >
+            {/* Nested routes for course modules */}
+            <Route path="courses/:courseId" element={<CourseLayout />} />
+            <Route path="courses/:courseId/:moduleId" element={<ModulePage />} />
+          </Route>
         </Routes>
       </Layout>
     </BrowserRouter>
